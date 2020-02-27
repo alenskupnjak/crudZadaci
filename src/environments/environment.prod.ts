534a9d018e16676a99aa7c14291bde34f2271a70
AlenS <alenskupnjak@yahoo.com>
2020-02-27 07:15:14 +0100

    005a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAjleQs-q3u5CnKlo9Kz35M0XJACzzd2MA',
+    authDomain: 'fir-demo-c8a69.firebaseapp.com',
+    databaseURL: 'https://fir-demo-c8a69.firebaseio.com',
+    projectId: 'fir-demo-c8a69',
+    storageBucket: 'fir-demo-c8a69.appspot.com',
+    messagingSenderId: '1050124365346',
+    appId: '1:1050124365346:web:29587023e89fa31cc6647b'
+  }
 };
